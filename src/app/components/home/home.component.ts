3e7ea4fb275380fde4a8682f2f115721e41347e0
brandonorjas <brandonorjas.lii@gmail.com>
2022-06-10 14:28:35 -0500

    Formulario Revision

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Articulo } from 'src/app/models/articulo.model';
+import { Revision } from 'src/app/models/revision.model';
 import { ArticulosService } from 'src/app/services/articulos.service';
 import { CambioInfoService } from 'src/app/services/cambio-info.service';
 import Swal from 'sweetalert2';
@@ -14,12 +15,16 @@ declare var $: any
 export class HomeComponent implements OnInit {
 
   articulo: Articulo;
+  revision:Revision;
 
   constructor(private articuloService: ArticulosService, private cambioInfoService: CambioInfoService) {
     this.articulo = new Articulo()
+    this.revision = new Revision();
+
   }
 
   ngOnInit(): void {
+    
     $(document).ready(function () {
       $('.fixed-action-btn').floatingActionButton({
         direction: 'left',
@@ -34,9 +39,21 @@ export class HomeComponent implements OnInit {
       $('#agregarArticulo').modal({ dismissible: false });
       $('#agregarArticulo').modal('open');
   }
+
+  agregarRevivision(){ 
+    console.log("CrearRevision");
+      $('#CrearRevision').modal({ dismissible: false });
+      $('##CrearRevision').modal('open');
+  }
 //creaar la Publicacion
 crearArticulo(articulos:any){
   console.log(articulos)
+}
+
+
+crearRevision(){
+  this.revision.idProfesor=Number(localStorage.getItem('idProfesor'));
+  console.log(this.revision);
 }
 
   enviarMensajeArticulo() {
---
+++ src/app/models/revision.model.ts
@@ -0,0 +1,8 @@
+export class Revision {
+	idRevision: number = 0;
+	idProfesor: number = 0;
+	tipoRp: string = '';
+	nombreRI: string = '';
+    fecha: string = '';
+    tituloRP: string = '';
+}
