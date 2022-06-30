caf9033e8fcc46b28ab95eeee9e59f2dfb9a044e
GoierBachus1 <95065275+GoierBachus1@users.noreply.github.com>
2022-06-30 17:27:40 -0500

    listTesistasV3

--- src/app/components/listar-tesista-jc/listar-tesista-jc.component.ts
+++ src/app/components/listar-tesista-jc/listar-tesista-jc.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ProfesorService } from 'src/app/services/profesor.service';
+import Swal from 'sweetalert2';
 import { TesistasService } from 'src/app/services/tesistas.service';
+import { Tesista } from 'src/app/models/tesista.model';
+declare var $: any;
+
 @Component({
   selector: 'app-listar-tesista-jc',
   templateUrl: './listar-tesista-jc.component.html',
@@ -8,10 +12,13 @@ import { TesistasService } from 'src/app/services/tesistas.service';
 })
 export class ListarTesistasJcComponent implements OnInit {
   tesistas: any[]
+  tesisActual:Tesista
 	fechaInicial: string;
 	fechaFinal: string;
 	idProfesor : number ;
 	profesores:any[];
+	idTesisActual:number
+	profesoresActuales: any []
 	idCarrera: any
 	// Paginación
 	pageSize = 10;
@@ -19,7 +26,10 @@ export class ListarTesistasJcComponent implements OnInit {
   constructor(private tesistasService: TesistasService, private profesorServices: ProfesorService) {
     let hoy = new Date()
 		this.tesistas = []
+		this.profesoresActuales=[]
 		this.profesores=[]
+		this.idTesisActual=1
+		this.tesisActual=new Tesista()
 		this.idProfesor= Number(localStorage.getItem('idProfesor'));
 		this.fechaInicial = `${hoy.getFullYear() - 1}-${('0' + (hoy.getMonth() + 1)).slice(-2)}-${('0' + hoy.getDate()).slice(-2)}`
 		this.fechaFinal = `${hoy.getFullYear()}-${('0' + (hoy.getMonth() + 1)).slice(-2)}-${('0' + hoy.getDate()).slice(-2)}`
@@ -50,5 +60,83 @@ export class ListarTesistasJcComponent implements OnInit {
 	}
 	convertirFecha(fecha: string) {
 		return new Date(fecha).toLocaleDateString("en-CA");
+	}
+	ModalPrioridades(idTesis:any,profesores: any) {
+		console.log("modalPrioridad");
+		this.idTesisActual = idTesis;
+		this.profesoresActuales = profesores;
+		$('#modalPrioridad').modal();
+		$('#modalPrioridad').modal('open');
+		console.log(this.profesoresActuales);
+	}
+	CambiarPrioridad() {
+		let aux: any[] = []
+		for(let i=0; i<this.profesoresActuales.length;i++){
+			aux.push({idProfesor:this.profesoresActuales[i].idProfesor,
+				pos:this.profesoresActuales[i].pos,
+				esInterno:this.profesoresActuales[i].esInterno})
+		}
+		this.tesistasService.updatePrioridadesTestistas(this.idTesisActual,aux).subscribe((resNewPos:any)=>{
+			Swal.fire({
+				position: 'center',
+				icon: 'success',
+				title: 'Prioridades Actualizadas',
+				showConfirmButton: false,
+				timer: 1000
+			  })
+			this.obtenerTesistas();
+		},
+			err => console.error(err))
+	}
+	ActualizarTesisModal(idTesis:any) {
+		console.log("tesis")
+		this.tesistasService.obtenerTesis(idTesis).subscribe((resArticulo:any)=>{
+			this.tesisActual=resArticulo;
+			console.log(this.tesisActual)
+			$('#ActualizarTesis').modal();
+			$('#ActualizarTesis').modal('open');
+		},
+			err => console.error(err))
+	}
+	actualizarArticulo() {
+		console.log(this.tesisActual)
+		this.tesistasService.actualizarTesis(this.tesisActual, this.tesisActual.idTesis).subscribe((resActualiza: any) => {
+			this.obtenerTesistas();
+			$('#ActualizarArticulo').modal('close');
+			Swal.fire({
+				position: 'center',
+				icon: 'success',
+				title: 'Articulo Modificado',
+				showConfirmButton: false,
+				timer: 1000
+			  })
+		},
+			err => console.error(err))
+	}
+	eliminarTesista(idTesis: number) {
+		Swal.fire({
+			title: '¿Estas seguro de querer eliminar el Articulo?',
+			position: 'center',
+			icon: 'question',
+			showDenyButton: true,
+			showConfirmButton: true,
+			confirmButtonText: 'Sí'
+		})
+		.then(respuesta => {
+				if (respuesta.isConfirmed) {
+					this.tesistasService.eliminarTesis(idTesis).subscribe({
+						next: (resEliminar: any) => {
+							Swal.fire({
+								position: 'center',
+								icon: 'success',
+								title: 'Articulo Eliminado',
+								showConfirmButton: false,
+								timer: 1000
+							  })
+							this.obtenerTesistas();
+						}
+					});
+				}
+		});
 	}
 }
--- src/app/services/tesistas.service.ts
+++ src/app/services/tesistas.service.ts
@@ -17,4 +17,16 @@ export class TesistasService {
   listTesistasByCarreraByPeriodo(idProfesor:number, fechaIni:any, fechaFin:any){
     return this.http.get(`${environment.API_URI}/tesistas/listTesistasByCarreraByPeriodo/${idProfesor}/${fechaIni}/${fechaFin}`);
   }
+  updatePrioridadesTestistas(idTesis:number, autores:any){
+    return this.http.get(`${environment.API_URI}/tesistas/updatePrioridadesTestistas/${idTesis}`,autores);
+  }
+  obtenerTesis(idTesis: number) {
+    return this.http.get(`${environment.API_URI}/tesistas/${idTesis}`);
+  }
+  actualizarTesis(tesis: any, idTesis: any) {
+    return this.http.put(`${environment.API_URI}/tesistas/update/${idTesis}`, tesis)
+  }
+  eliminarTesis(idTesis:any){
+    return this.http.delete(`${environment.API_URI}/tesistas/delete/${idTesis}`);
+  }
 }
